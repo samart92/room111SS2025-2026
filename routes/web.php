0e9cee7a00a13e9544312a4a7c6f2b46d91b7b5b
Horn Sam Art <hornsamart@gmail.com>
2026-05-16 17:36:53 +0700

    onlineproduct with admin

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+
+// $viewData = [];
+// $viewData["title"] = "Home Page - Online Store";
+// return view('home.index')->with("viewData", $viewData);
+// });
+// Route::get('/', [HomeController::class,'index'])->name("home.about");
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
