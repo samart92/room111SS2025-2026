d5b8288b634766ee368a17c0b89f34778892d435
Horn Sam Art <hornsamart@gmail.com>
2026-05-23 16:41:09 +0700

    addproduct complect

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -5,15 +5,42 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminProductController extends Controller
 {
     public function index()
- {
-  $viewData = [];
-  $viewData["title"] = "Admin Page - Products - Online Store";
-  $viewData["products"] = Product::all();
-  return view('admin.product.index')->with("viewData", $viewData);
- }
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Products - Online Store";
+        $viewData["products"] = Product::all();
+        return view('admin.product.index')->with("viewData", $viewData);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|max:255",
+            "description" => "required",
+            "price" => "required|numeric|gt:0",
+            'image' => 'image',
+        ]);
+        $newProduct = new Product();
+        $newProduct->setName($request->input('name'));
+        $newProduct->setDescription($request->input('description'));
+        $newProduct->setPrice($request->input('price'));
+        $newProduct->setImage("game.png");
+        $newProduct->save();
 
+        if ($request->hasFile('image')) {
+            $imageName = $newProduct->getId() . "." . $request->file('image')->extension();
+            Storage::disk('public')->put(
+                $imageName,
+                file_get_contents($request->file('image')->getRealPath())
+            );
+            $newProduct->setImage($imageName);
+            $newProduct->save();
+        }
+
+        return back();
+    }
 }
